49db23809ff1670259d66cd9477897436e02d47b
Eugene Petkevich <nasedil.genio.code@gmail.com>
2018-07-01 23:20:02 +0300

    Add command-line options in tool for date to parse and birth date

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Work is in the earliest stage of development.
 
 ## TODO list:
-### Calndar itself
+### Calendar itself
  * What to do with dates before birth date?
  * How to handle days after death?
  * Add calendar description
@@ -20,9 +20,14 @@ Work is in the earliest stage of development.
  * make installable package
 
 ### Other stuff
- * make library in javascript (well, initially thought of making this is javascript, python version is just a coincidence, because I was too lazy for couple of years and forgot)
+ * make library in JavaScript (well, initially thought of making this is JavaScript, python version is just a coincidence, because I was too lazy for couple of years and forgot)
  * Add links to projects related to the calendar
 
 ## Short notes
 
 Sometimes I use a system to write a date with origin as my date of birth.  So I write this software to automatically convert dates to/from my calendar.
+
+## Version history
+
+### v0.0.1 -- 2018-07-01 -- First versioned version.
+ * Add command-line options in tool for date to parse and birth date.
--- sevencal/sevencal.py
+++ sevencal/sevencal.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python
+from argparse import ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser
 import datetime
 
 import sevencal.sevencalendar as sevencalendar
 
+def date_type(s):
+    """Convert string to datetime.
+    """
+    return datetime.datetime.strptime(s, '%Y-%m-%d').date()
+
 def main():
-    """Helper tool for sever-calendar, prints date of today."""
-    today = datetime.date.today()
-    print(sevencalendar.SevenDate.from_date(today))
+    """Helper command-line tool for sever-calendar.."""
+    parser = ArgumentParser(description='777-calendar converter tool',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d', '--date',
+                        type=date_type,
+                        default=datetime.date.today(),
+                        help='The date to convert (in format yyyy-mm-dd)')
+    parser.add_argument('-b', '--birth-date',
+                        type=date_type,
+                        default=datetime.date(1988, 6, 23),
+                        help='The reference (birth) date of the calendar (in format yyyy-mm-dd)')
+    args = parser.parse_args()
+    print(sevencalendar.SevenDate.from_date(args.date, birth_date=args.birth_date))
 
 if __name__ == '__main__':
     main()
